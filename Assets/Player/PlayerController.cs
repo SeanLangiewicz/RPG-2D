9ac0804b23558dc3c6d1f49b4d05b9f87d88574c
Sean <slangiewicz.21@gmail.com>
2017-07-17 19:15:03 -0700

    EP3

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -7,11 +7,12 @@ public class PlayerController : MonoBehaviour
     public float movementSpeed;
     public float velocity;
     public Rigidbody rb;
-
+    public Animator anim;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        
 	}
 	
 	// Update is called once per frame
@@ -19,6 +20,7 @@ public class PlayerController : MonoBehaviour
     {
         GetInput();
         Move();
+       
 	}
 
     void GetInput()
@@ -46,12 +48,32 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void Move()
+   
+        void Move()
     {
+        if(velocity ==0)
+        {
+            anim.SetInteger("Condition", 0);
+            return;
+        }
+        else
+        {
+            anim.SetInteger("Condition", 1);
+        }
         rb.MovePosition(transform.position + (Vector3.right*velocity * movementSpeed*Time.deltaTime));    
     }
     void SetVelocity(float dir)
     {
-        velocity = dir;
+            if (dir < 0)
+                {
+            transform.LookAt(transform.position + Vector3.left);
+                }
+
+        else if (dir > 0)
+        {
+            transform.LookAt(transform.position + Vector3.right);
+                velocity = dir;
+        }
+        
     }
 }
